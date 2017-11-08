90e2083dae02cd00f041f538dbb82ada99e58810
Ignacy Rzecki <dkinvoker@gmail.com>
2017-11-08 17:28:56 +0100

    Project has been initialized

---
+++ DroneClient/DroneClient/ClientManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DroneClient
+{
+    class ClientManager
+    {
+        private TcpClient _tcpClient = new TcpClient();
+
+        //This method establishes a connection between client and the server app
+        public void ConnectToTheServer(IPAddress ServerAdress, int port)
+        {
+            _tcpClient.Connect(ServerAdress, port);
+        }
+
+    }
+}
---
+++ DroneClient/DroneClient/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DroneClient
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ClientManager _clientManager = new ClientManager();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ConnectionTestButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IPAddress adress = IPAddress.Parse(this.AdressTextBox.Text);
+                int port = int.Parse(this.PortTextBox.Text);
+                _clientManager.ConnectToTheServer(adress, port);
+            }
+            catch (Exception exception)
+            {
+                this.ErrorTextBlock.Text = exception.Message;
+            }
+
+        }
+    }
+}
