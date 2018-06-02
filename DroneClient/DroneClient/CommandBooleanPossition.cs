6e5269fdd2caf6fb8cbaa7d2b31c9799353384f2
roosa123 <dianka.windsurfer@op.pl>
2018-06-03 00:52:19 +0200

    modified main window, added new functionality

---
+++ DroneClient/DroneClient/CommandBooleanPossition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AR.Drone.WinApp
+{
+    class CommandBooleanPossition
+    {
+        public const int TakeoffIndex = 0;
+        public const int ResetEmergencyIndex = 1;
+        public const int EmergencyIndex = 2;
+        public const int HoverIndex = 3;
+        public const int LandIndex = 4;
+        public const int StopIndex = 5;
+        public const int StartIndex = 6;
+
+    }
+}
--- DroneClient/DroneClient/MainWindow.xaml.cs
+++ DroneClient/DroneClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AR.Drone.WinApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -25,8 +26,11 @@ namespace DroneClient
     {
         private Socket _socket = null;
         private Timer _reciveTimer = null;
-        private byte[] _reciveByteArray = new byte[32];
-        private Int16[] _reciveAnalogs = new Int16[16];
+        private byte[] _reciveByteArray = new byte[_howManyAnalogs * sizeof(float) + _howManyCommandsBytes];
+        private float[] _reciveAnalogs = new float[4];
+        private const int _howManyCommandsBytes = 1;
+        private const int _howManyAnalogs = 4;
+        private byte[] _commandsArrayAsBytes = new byte[_howManyCommandsBytes];
 
         public MainWindow()
         {
@@ -57,23 +61,115 @@ namespace DroneClient
         {
             try
             {
-                this.FromSimulatorTextBlock.Text = string.Empty;
+                string infoToAnalogDisplay = string.Empty;
+
+                _reciveTimer.Stop();
                 _socket.Receive(_reciveByteArray);
-                //Konwersja bajtów na inty16-bitowe
-                for (int i = 0; i < _reciveByteArray.Length; i+=2)
+                //Konwersja bajtów na floaty
+                for (int i = 0; i < _howManyAnalogs * sizeof(float); i += sizeof(float))
                 {
-                    _reciveAnalogs[i / 2] = BitConverter.ToInt16(_reciveByteArray, i);
-                    this.FromSimulatorTextBlock.Text = i + ": " + _reciveAnalogs[i / 2];
+                    _reciveAnalogs[i / sizeof(float)] = BitConverter.ToSingle(_reciveByteArray, i);                   
                 }
 
+                infoToAnalogDisplay += "roll: " + _reciveAnalogs[0] + "\n";
+                infoToAnalogDisplay += "pitch: " + _reciveAnalogs[1] + "\n";
+                infoToAnalogDisplay += "gaz: " + _reciveAnalogs[2] + "\n";
+                infoToAnalogDisplay += "yaw: " + _reciveAnalogs[3] + "\n";
+
+                //Wyodrębnianie komend
+                for (int i = _howManyAnalogs * sizeof(float); i < _reciveByteArray.Length; ++i)
+                {
+                    _commandsArrayAsBytes[i - _howManyAnalogs * sizeof(float)] = _reciveByteArray[i];
+                }
+                #region TEST
+                ////-----W CELACH TESTOWYCH-----
+                //if (_commandsArrayAsBytes[0] != 0)
+                //{
+                //    int i = 10;
+                //}
+                ////----------------------------
+                #endregion
+
+                string intoToCommandDisplay = CommandsText(BytesIntoBooleanArray(_commandsArrayAsBytes));
+
+                //Ten dispaczer jest po to, aby ten wątek miał dostęp do GUI
+                this.Dispatcher.Invoke(() => { this.FromSimulatorTextBlock.Text = infoToAnalogDisplay; });
+                if (intoToCommandDisplay != string.Empty)
+                {
+                    this.Dispatcher.Invoke(() => { this.FromSimulatorCommandsTextBlock.Text = intoToCommandDisplay; });
+                }
+                ////-----W CELACH TESTOWYCH-----
+                //else
+                //{
+                //    this.Dispatcher.Invoke(() => { this.FromSimulatorCommandsTextBlock.Text = "*" +  this.FromSimulatorCommandsTextBlock.Text; });
+                //}
+                ////----------------------------
+
+                _reciveTimer.Start();
                 
 
             }
             catch (Exception exception)
             {
+                //Gdyby się nie zastartował w traju
+                _reciveTimer.Start();
+                this.Dispatcher.Invoke(() => { this.FromSimulatorTextBlock.Text = exception.Message; });
                 //Narazie zlewam tę sytuację, albowiem synchronizacja wątków i wgl nie działa
                 //this.LogTextBlock.Text = exception.Message;
             }
+        }
+
+        private Boolean[] BytesIntoBooleanArray(byte[] commandsArray)
+        {
+            Boolean[] returner = new Boolean[commandsArray.Length * 8];
+            int WhihByte = -1;
+
+            for (int i = 0; i < returner.Length; ++i)
+            {
+                if (i % 8 == 0)
+                {
+                    ++WhihByte;
+                }
+                returner[i] = (commandsArray[WhihByte] & (1 << i % 8)) == 1 << i % 8;
+            }
+
+            return returner;
+        }
+
+        private string CommandsText(Boolean[] commands)
+        {
+            string returner = string.Empty;
+
+            if (commands[CommandBooleanPossition.EmergencyIndex])
+            {
+                returner += "Emergency; ";
+            }
+            if (commands[CommandBooleanPossition.HoverIndex])
+            {
+                returner += "Hover; ";
+            }
+            if (commands[CommandBooleanPossition.LandIndex])
+            {
+                returner += "Land; ";
+            }
+            if (commands[CommandBooleanPossition.ResetEmergencyIndex])
+            {
+                returner += "ResetEmergency; ";
+            }
+            if (commands[CommandBooleanPossition.StartIndex])
+            {
+                returner += "Start; ";
+            }
+            if (commands[CommandBooleanPossition.StopIndex])
+            {
+                returner += "Stop; ";
+            }
+            if (commands[CommandBooleanPossition.TakeoffIndex])
+            {
+                returner += "Takeoff; ";
+            }
+
+            return returner;
         }
 
     }
