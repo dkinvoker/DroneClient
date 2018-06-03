8bf87c88f863cd99cb6f1e4b748b87a618134e3d
Artur <artur.swoboda@gorrion.pl>
2018-06-03 22:16:30 +0200

    naming fixes, split code

--- DroneClient/DroneClient/MainWindow.xaml.cs
+++ DroneClient/DroneClient/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DroneClient.Helpers;
+using DroneClient.DroneHandle;
 
 namespace DroneClient
 {
@@ -25,17 +27,20 @@ namespace DroneClient
     public partial class MainWindow : Window
     {
         private Socket _socket = null;
-        private Timer _reciveTimer = null;
-        private byte[] _reciveByteArray = new byte[_howManyAnalogs * sizeof(float) + _howManyCommandsBytes];
-        private float[] _reciveAnalogs = new float[4];
+        private Timer _receiveTimer = null;
+        private byte[] _receiveByteArray = new byte[_howManyAnalogs * sizeof(float) + _howManyCommandsBytes];
+        private float[] _receiveAnalogs = new float[4];
         private const int _howManyCommandsBytes = 1;
         private const int _howManyAnalogs = 4;
         private byte[] _commandsArrayAsBytes = new byte[_howManyCommandsBytes];
 
         public MainWindow()
         {
+            Byte[] test = new byte[2] { 0b0000_1111, 0b0011_1100 };
+            test.BytesIntoBooleanArray();
+
             InitializeComponent();
-            _reciveTimer = new Timer(100);
+            _receiveTimer = new Timer(100);
         }
 
         private void ConnectionTestButton_Click(object sender, RoutedEventArgs e)
@@ -47,8 +52,8 @@ namespace DroneClient
                 int port = int.Parse(this.PortTextBox.Text);
                 _socket.Connect(adress, port);
                 LogTextBlock.Text = "Działa!";
-                _reciveTimer.Elapsed += _reciveTimer_Elapsed;
-                _reciveTimer.Start();
+                _receiveTimer.Elapsed += _receiveTimer_Elapsed;
+                _receiveTimer.Start();
             }
             catch (Exception exception)
             {
@@ -57,29 +62,29 @@ namespace DroneClient
 
         }
 
-        private void _reciveTimer_Elapsed(object sender, ElapsedEventArgs e)
+        private void _receiveTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
             try
             {
                 string infoToAnalogDisplay = string.Empty;
 
-                _reciveTimer.Stop();
-                _socket.Receive(_reciveByteArray);
+                _receiveTimer.Stop();
+                _socket.Receive(_receiveByteArray);
                 //Konwersja bajtów na floaty
                 for (int i = 0; i < _howManyAnalogs * sizeof(float); i += sizeof(float))
                 {
-                    _reciveAnalogs[i / sizeof(float)] = BitConverter.ToSingle(_reciveByteArray, i);                   
+                    _receiveAnalogs[i / sizeof(float)] = BitConverter.ToSingle(_receiveByteArray, i);                   
                 }
 
-                infoToAnalogDisplay += "roll: " + _reciveAnalogs[0] + "\n";
-                infoToAnalogDisplay += "pitch: " + _reciveAnalogs[1] + "\n";
-                infoToAnalogDisplay += "gaz: " + _reciveAnalogs[2] + "\n";
-                infoToAnalogDisplay += "yaw: " + _reciveAnalogs[3] + "\n";
+                infoToAnalogDisplay += "roll: " + _receiveAnalogs[0] + "\n";
+                infoToAnalogDisplay += "pitch: " + _receiveAnalogs[1] + "\n";
+                infoToAnalogDisplay += "gaz: " + _receiveAnalogs[2] + "\n";
+                infoToAnalogDisplay += "yaw: " + _receiveAnalogs[3] + "\n";
 
                 //Wyodrębnianie komend
-                for (int i = _howManyAnalogs * sizeof(float); i < _reciveByteArray.Length; ++i)
+                for (int i = _howManyAnalogs * sizeof(float); i < _receiveByteArray.Length; ++i)
                 {
-                    _commandsArrayAsBytes[i - _howManyAnalogs * sizeof(float)] = _reciveByteArray[i];
+                    _commandsArrayAsBytes[i - _howManyAnalogs * sizeof(float)] = _receiveByteArray[i];
                 }
                 #region TEST
                 ////-----W CELACH TESTOWYCH-----
@@ -90,7 +95,7 @@ namespace DroneClient
                 ////----------------------------
                 #endregion
 
-                string intoToCommandDisplay = CommandsText(BytesIntoBooleanArray(_commandsArrayAsBytes));
+                string intoToCommandDisplay = Drone.CommandsText(_commandsArrayAsBytes.BytesIntoBooleanArray());
 
                 //Ten dispaczer jest po to, aby ten wątek miał dostęp do GUI
                 this.Dispatcher.Invoke(() => { this.FromSimulatorTextBlock.Text = infoToAnalogDisplay; });
@@ -105,72 +110,17 @@ namespace DroneClient
                 //}
                 ////----------------------------
 
-                _reciveTimer.Start();
-                
-
+                _receiveTimer.Start();
+         
             }
             catch (Exception exception)
             {
                 //Gdyby się nie zastartował w traju
-                _reciveTimer.Start();
+                _receiveTimer.Start();
                 this.Dispatcher.Invoke(() => { this.FromSimulatorTextBlock.Text = exception.Message; });
                 //Narazie zlewam tę sytuację, albowiem synchronizacja wątków i wgl nie działa
                 //this.LogTextBlock.Text = exception.Message;
             }
         }
-
-        private Boolean[] BytesIntoBooleanArray(byte[] commandsArray)
-        {
-            Boolean[] returner = new Boolean[commandsArray.Length * 8];
-            int WhihByte = -1;
-
-            for (int i = 0; i < returner.Length; ++i)
-            {
-                if (i % 8 == 0)
-                {
-                    ++WhihByte;
-                }
-                returner[i] = (commandsArray[WhihByte] & (1 << i % 8)) == 1 << i % 8;
-            }
-
-            return returner;
-        }
-
-        private string CommandsText(Boolean[] commands)
-        {
-            string returner = string.Empty;
-
-            if (commands[CommandBooleanPossition.EmergencyIndex])
-            {
-                returner += "Emergency; ";
-            }
-            if (commands[CommandBooleanPossition.HoverIndex])
-            {
-                returner += "Hover; ";
-            }
-            if (commands[CommandBooleanPossition.LandIndex])
-            {
-                returner += "Land; ";
-            }
-            if (commands[CommandBooleanPossition.ResetEmergencyIndex])
-            {
-                returner += "ResetEmergency; ";
-            }
-            if (commands[CommandBooleanPossition.StartIndex])
-            {
-                returner += "Start; ";
-            }
-            if (commands[CommandBooleanPossition.StopIndex])
-            {
-                returner += "Stop; ";
-            }
-            if (commands[CommandBooleanPossition.TakeoffIndex])
-            {
-                returner += "Takeoff; ";
-            }
-
-            return returner;
-        }
-
     }
 }
